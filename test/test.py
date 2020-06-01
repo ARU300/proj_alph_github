1ad41582080a03a31327333e271cadc7744aa275
Hacker_Boiii <ARU300@habsboys.org,uk>
2020-06-01 16:26:33 +0100

    a

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 from sklearn import datasets
 import pandas as pd
-iris = datasets.load_iris
+iris = datasets.load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
 print(df)
 import revoscalepy
