5682bb0634ffeea4260535f03487992cac1b40ef
Hacker_Boiii <ARU300@habsboys.org,uk>
2020-06-01 15:41:03 +0100

    b

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 iris = datasets.load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
 print(df)
-#import revoscalpey
+import revoscalpey
 from revoscalepy import rx_summary
 summary = rx_summary("petal length (cm)", df)
 print(summary)
